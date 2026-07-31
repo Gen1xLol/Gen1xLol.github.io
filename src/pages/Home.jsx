5ccb4b8fb7c641085f15d3865f377d207438701d
Gen1xLol <gen1x.01.cool@gmail.com>
2026-07-31 06:25:12 -0300

    a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -85,7 +85,8 @@ export default function Home() {
             I'm listed as <span>G1nX</span> on the PenguinMod Extra Gallery.
             <br /><br />
             I really like the challenging parts in programming. I like looking at things from different angles, trying to piece things together. I hope this doesn't sound pretentious lol
-            I also like playing videogames from time to time. I'm REALLY into UNDERTALE and DELTARUNE :D
+            <br />
+			I also like playing videogames from time to time. I'm REALLY into UNDERTALE and DELTARUNE :D
 			<br /><br />
             I'm also a "writer" <small style={{ color: 'var(--soft)' }}>(I don't have much experience...)</small>
             <br />Here's <a href="https://bit.ly/4vrdkiM" target="_blank" rel="noopener">my poetry collection</a> if you want to see what I've written so far.
