1cd51c0962fc84c7a7f03e66116bcc4a43fdeebe
Gen1xLol <gen1x.01.cool@gmail.com>
2026-08-26 12:41:38 -0300

    siiite update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -867,7 +867,7 @@ export default function Home() {
             <ThoughtBubble text={thought} onClick={cycleThought} shapeSeed={bubbleShapeSeed} />
           </div>
           <div className="fade-in" style={{ animationDelay: '1.4s' }}>
-            <p className="prev">also known as <span>G1nX</span> / <span>YoSoyGena</span></p>
+            <p className="prev">AKA: <span><a href="https://github.com/Gen1xLol" target="_blank" rel="noopener">Gen1xLol</a></span> / <span>YoSoyGena</span> / <span>G1nX</span> (very ocasionally)</p>
             <div className="age-line">
               <span className="age-num" id="age-display">{age}</span>
               <span style={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
